29b0d439253d9a7a7315ee46570d2db87a742066
Nate Montgomery <kilikalbright@hotmail.com>
2019-11-30 08:50:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
-def addition(num1, num2)
 
+
+def addition(num1, num2)
+sum  = num1 + num2
+puts sum
 end
 
 def subtraction(num1, num2)
-
+sub_sum = num1 - num2
+puts sub_sum
 end
 
 def division(num1, num2)
-
+quotient = num2 / num1
+puts quotient
 end
 
 def multiplication(num1, num2)
-
+times_sum = num1 * num2
+puts times_sum
 end
 
 def modulo(num1, num2)
-
+modulo_sum = num2 % num1
+puts modulo_sum
 end
 
 def square_root(num)
